675cc1b3672a9794f49668b7d0e0f9f663db89a9
Dino <ngohongthai.uet@gmail.com>
2025-08-18 11:09:44 +0700

    remove unused code

--- src/evaluation_pipeline/inference/__init__.py
+++ src/evaluation_pipeline/inference/__init__.py
@@ -2,10 +2,8 @@
 Inference modules for the evaluation pipeline.
 """
 
-from .gemma3n_inference import Gemma3NInferenceEngine
-from .batch_inference import BatchInferenceEngine
+from .inference_engine import InferenceEngine
 
 __all__ = [
-    "Gemma3NInferenceEngine",
-    "BatchInferenceEngine",
+    "InferenceEngine"
 ]
